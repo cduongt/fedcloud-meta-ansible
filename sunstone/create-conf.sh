d0c2e5c83fb40b65fe8dfd5c9701528b18ba4578
cduongt <cduongt@gmail.com>
2017-12-06 00:04:12 +0100

    fixed this terrible syntax for file secret

--- sunstone/create-conf.sh
+++ sunstone/create-conf.sh
@@ -31,4 +31,4 @@ cat >/etc/one/sunstone-server.conf << EOL
     - support
 EOL
 
-echo "service:${SUNSTONE_SERVERADMIN_SECRET}" >> /var/lib/one/.one/sunstone_auth
+echo "serveradmin:$(cat "$SUNSTONE_SERVERADMIN_SECRET")" >> /var/lib/one/.one/sunstone_auth
